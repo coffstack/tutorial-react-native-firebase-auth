5ef94d66eeaae148a9d4c6770c44b51bca3c8e23
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-03-18 19:39:05 -0300

    signUp call

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {View, Image} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 import {styles} from './styles';
 
@@ -11,6 +12,25 @@ import {MyLink} from '../components/MyLink';
 export function SignInScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  function signUp() {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('User account created & signed in!');
+      })
+      .catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
+  }
 
   return (
     <View style={[styles.container, {justifyContent: 'center'}]}>
@@ -25,7 +45,7 @@ export function SignInScreen() {
 
       <MyButton title="Entrar no App" />
 
-      <MyLink>Cadastrar</MyLink>
+      <MyLink title="Cadastrar" onPress={signUp} />
     </View>
   );
 }
