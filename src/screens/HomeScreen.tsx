eb90c741f658f46dc47c40dba0dec3e978084cc5
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-03-19 13:31:55 -0300

    signOut

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 import {MyButton} from '../components/MyButton';
 import {styles} from './styles';
 
 export function HomeScreen() {
+  function signOut() {
+    auth().signOut();
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
         Essa tela só pode ser vista por usuários autenticados
       </Text>
-      <MyButton title="Sair" />
+      <MyButton onPress={signOut} title="Sair" />
       <Text>
         by <Text style={styles.coffText}>Coffstack</Text>
       </Text>
