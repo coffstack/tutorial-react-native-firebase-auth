cc6237a276d7a32174fa8ac7c20d20a87081bbd0
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-03-19 13:31:48 -0300

    handle user state change

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+
+import {ActivityIndicator, SafeAreaView, View} from 'react-native';
 import {HomeScreen} from './src/screens/HomeScreen';
 
 import {SignInScreen} from './src/screens/SignInScreen';
+import {colors} from './src/colors';
 
 const App = () => {
-  const user = false;
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged(_user => {
+      if (initializing) {
+        setInitializing(false);
+      }
+      setUser(_user);
+    });
+
+    return unsubscribe;
+  }, [initializing]);
+
+  if (initializing) {
+    return (
+      <View>
+        <ActivityIndicator color={colors.primary} />
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView style={{flex: 1}}>
       {user ? <HomeScreen /> : <SignInScreen />}
